4514e4195a944b57b610b951087935edfc25619b
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-18 22:01:36 -0300

    feat: get by tag and id

--- javascript-navegador/03_mover_no_dom/js/script.js
+++ javascript-navegador/03_mover_no_dom/js/script.js
@@ -1,5 +1,5 @@
-console.log(document.body)
+console.log(document.body);
 
-console.log(document.body.childNodes[1])
-console.log(document.body.childNodes[1].childNodes[1])
-console.log(document.body.childNodes[1].childNodes[1].innerText)
+console.log(document.body.childNodes[1]);
+console.log(document.body.childNodes[1].childNodes[1]);
+console.log(document.body.childNodes[1].childNodes[1].innerText);
