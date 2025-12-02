a3dc80b51cfc06bfc5561171d4160c511d46cdc5
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-02 20:59:42 -0300

    feat: propagation e stopPropagation()

---
+++ eventos/04_propagation/js/script.js
@@ -0,0 +1,20 @@
+let btn1 = document.querySelector('#btn1')
+let btn2 = document.querySelector('#btn2')
+let p = document.querySelector('p')
+
+console.log(btn1)
+
+function msg(e) {
+  console.log('Clicou no button')
+  e.stopPropagation()
+}
+
+btn1.addEventListener('click', msg)
+
+btn2.addEventListener('click', function (event) {
+  console.log(event)
+})
+
+p.addEventListener('click', function () {
+  console.log('clicou no paragrafo')
+})
