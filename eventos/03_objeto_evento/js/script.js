724e0bfc947ec981e0d28b63a44ee41d5e7d31b5
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-02 00:02:22 -0300

    feat: objeto event

---
+++ eventos/03_objeto_evento/js/script.js
@@ -0,0 +1,14 @@
+let btn1 = document.querySelector('#btn1')
+let btn2 = document.querySelector('#btn2')
+
+console.log(btn1)
+
+function msg(e) {
+  console.log(e)
+}
+
+btn1.addEventListener('click', msg)
+
+btn2.addEventListener('click', function (event) {
+  console.log(event)
+})
