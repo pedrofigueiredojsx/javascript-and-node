10e778e09282331f6624a881519fe593bc39d8d3
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-14 15:28:45 -0300

    feat: win condition function created

--- jogo-da-velha/js/script.js
+++ jogo-da-velha/js/script.js
@@ -70,10 +70,10 @@ function checkWinCondition() {
 
     if (b1Child === 'x' && b2Child === 'x' && b3Child === 'x') {
       // x
-      console.log('X Venceu')
+      declareWinner('x')
     } else if (b1Child === 'o' && b2Child === 'o' && b3Child === 'o') {
       // o
-      console.log('O Venceu')
+      declareWinner('o')
     }
   }
 
@@ -84,10 +84,10 @@ function checkWinCondition() {
 
     if (b4Child === 'x' && b5Child === 'x' && b6Child === 'x') {
       // x
-      console.log('X Venceu')
+      declareWinner('x')
     } else if (b4Child === 'o' && b5Child === 'o' && b6Child === 'o') {
       // o
-      console.log('O Venceu')
+      declareWinner('o')
     }
   }
 
@@ -98,10 +98,10 @@ function checkWinCondition() {
 
     if (b7Child === 'x' && b8Child === 'x' && b9Child === 'x') {
       // x
-      console.log('X Venceu')
+      declareWinner('x')
     } else if (b7Child === 'o' && b8Child === 'o' && b9Child === 'o') {
       // o
-      console.log('O Venceu')
+      declareWinner('o')
     }
   }
 
@@ -113,10 +113,10 @@ function checkWinCondition() {
 
     if (b1Child === 'x' && b4Child === 'x' && b7Child === 'x') {
       // x
-      console.log('X Venceu')
+      declareWinner('x')
     } else if (b1Child === 'o' && b4Child === 'o' && b7Child === 'o') {
       // o
-      console.log('O Venceu')
+      declareWinner('o')
     }
   }
 
@@ -127,10 +127,10 @@ function checkWinCondition() {
 
     if (b1Child === 'x' && b4Child === 'x' && b7Child === 'x') {
       // x
-      console.log('X Venceu')
+      declareWinner('x')
     } else if (b1Child === 'o' && b4Child === 'o' && b7Child === 'o') {
       // o
-      console.log('O Venceu')
+      declareWinner('o')
     }
   }
 
@@ -141,10 +141,10 @@ function checkWinCondition() {
 
     if (b2Child === 'x' && b5Child === 'x' && b8Child === 'x') {
       // x
-      console.log('X Venceu')
+      declareWinner('x')
     } else if (b2Child === 'o' && b5Child === 'o' && b8Child === 'o') {
       // o
-      console.log('O Venceu')
+      declareWinner('o')
     }
   }
 
@@ -155,10 +155,10 @@ function checkWinCondition() {
 
     if (b3Child === 'x' && b6Child === 'x' && b9Child === 'x') {
       // x
-      console.log('X Venceu')
+      declareWinner('x')
     } else if (b3Child === 'o' && b6Child === 'o' && b9Child === 'o') {
       // o
-      console.log('O Venceu')
+      declareWinner('o')
     }
   }
 
@@ -171,10 +171,10 @@ function checkWinCondition() {
 
     if (b1Child === 'x' && b5Child === 'x' && b9Child === 'x') {
       // x
-      console.log('X Venceu')
+      declareWinner('x')
     } else if (b1Child === 'o' && b5Child === 'o' && b9Child === 'o') {
       // o
-      console.log('O Venceu')
+      declareWinner('o')
     }
   }
 
@@ -185,10 +185,10 @@ function checkWinCondition() {
 
     if (b3Child === 'x' && b5Child === 'x' && b7Child === 'x') {
       // x
-      console.log('X Venceu')
+      declareWinner('x')
     } else if (b3Child === 'o' && b5Child === 'o' && b7Child === 'o') {
       // o
-      console.log('O Venceu')
+      declareWinner('o')
     }
   }
 
@@ -202,6 +202,45 @@ function checkWinCondition() {
   }
 
   if (counter == 9) {
-    console.log('deu velha')
+    declareWinner('deu velha')
   }
+}
+
+// limpa o jogo, declara o vencedor e atualiza o placar
+function declareWinner(winner) {
+
+  let scoreboardX = document.querySelector('#scoreboard-1')
+  let scoreboardY = document.querySelector('#scoreboard-2')
+  let msg = ''
+
+  if (winner == 'x') {
+    scoreboardX.textContent = parseInt(scoreboardX.textContent) + 1
+    msg = 'O jogador 1 venceu!'
+  } else if (winner == 'o') {
+    scoreboardY.textContent = parseInt(scoreboardY.textContent) + 1
+    msg = 'O jogador 2 venceu!'
+  } else {
+    msg = 'Deu velha!'
+  }
+
+  // exibe msg
+  messageText.innerHTML = msg
+  messageContainer.classList.remove('hide')
+
+  // esconde msg
+  setTimeout(function () {
+    messageContainer.classList.add('hide')
+  }, 3000)
+
+  // zera as jogadas
+  player1 = 0
+  player2 = 0;
+
+  // remove x e o
+  let boxesToRemove = document.querySelectorAll('.box div')
+
+  for (let i = 0; i < boxesToRemove.length; i++) {
+    boxesToRemove[i].parentNode.removeChild(boxesToRemove[i])
+  }
+
 }
