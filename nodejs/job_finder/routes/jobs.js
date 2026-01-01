318ef4cf88fd13c91c39737c4ff8c41eebe54f2a
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2026-01-01 17:43:06 -0300

    feat: routes

---
+++ nodejs/job_finder/routes/jobs.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const Job = require('../models/Job')
+
+// ad job via post
+router.post('/add', (req, res) => {
+  let { title, salary, company, description, email, new_job } = req.body
+
+  // insert
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+  })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
+module.exports = router
